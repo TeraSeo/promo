66497d9936eb5d31fa55f8c7b6907cc7ccfbf3d7
TeraSeo <96968917+TeraSeo@users.noreply.github.com>
2024-02-12 01:19:01 +0900

    Update constants.dart

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String appId = "1:950629376783:web:836f50805e700d5768d47a";
-  static String apiKey = "AIzaSyD_ql5P6jcGtYGVIi2_zgsKrRVPxjH7diE";
-  static String messagingSenderId = "950629376783";
-  static String projectId = "likeapp-adff4";
+  static String appId = "";
+  static String apiKey = "";
+  static String messagingSenderId = "";
+  static String projectId = "";
 
   final primaryColor = Color.fromARGB(255, 0, 0, 0);
   final iconColor = Colors.white;
